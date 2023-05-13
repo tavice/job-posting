3d75babc903942d145950cc06ad0987c090ab11e
tavice <thomasavice.ta@gmail.comm>
2023-05-13 16:49:23 -0700

    started displaying more info still need to filter per employer minor css update

--- frontend/src/pages/EmployerList.js
+++ frontend/src/pages/EmployerList.js
@@ -1,37 +1,102 @@
-import { useEffect } from "react";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import axios from "axios";
 
 const EmployerList = ({ baseUrl }) => {
-
-  //Fetch data from backend using axios and useEffect
   const [employerList, setEmployerList] = useState([]);
 
+  //=======================================================//
+  // Fetching Employer List//
   const fetchEmployerList = async () => {
     const response = await axios.get(`${baseUrl}/api/employers/?format=json`);
     const data = response.data;
     setEmployerList(data);
   };
 
-  console.log(employerList);
-
   useEffect(() => {
     fetchEmployerList();
   }, []);
 
+  console.log(employerList);
+  //=======================================================//
+  // Fetching Job Listings//
+  const fetchJobListings = async (employerId) => {
+    try {
+      const response = await axios.get(`${baseUrl}/api/joblistings/?employer=${employerId}`);
+      const data = response.data;
+      return data;
+    } catch (error) {
+      console.log(error);
+      return [];
+    }
+  };
+
+  const [jobListings, setJobListings] = useState({});
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const jobListingsData = {};
+      for (const employer of employerList) {
+        const jobs = await fetchJobListings(employer.id);
+        console.log(jobs);
+        jobListingsData[employer.id] = jobs;
+      }
+      setJobListings(jobListingsData);
+    };
+    fetchData();
+  }, [employerList]);
+
+  console.log(jobListings);
+
+  //=======================================================//
+
   return (
     <div>
       <h1>Employer List</h1>
-      <ul>
-        {employerList.map((employer) => {
-          return (
-            <li key={employer.id}>
-              <h2>{employer.companyname}</h2>
-              <p>{employer.website}</p>
-            </li>
-          );
-        })}
-      </ul>
+      {employerList.map((employer) => (
+        <section key={employer.id}>
+          <div className="container">
+            <div className="employer-info">
+              <h1>{employer.companyname}</h1>
+              <div className="logo-container">
+                <img src={employer.logo} alt="Company Logo" />
+              </div>
+              <p>
+                <strong>Website:</strong>{" "}
+                <a href={employer.website}>{employer.companyname}</a>
+              </p>
+              <p>
+                <strong>Location:</strong> {employer.location}
+              </p>
+            </div>
+            <div className="job-listings">
+              <h2>Job Listings</h2>
+              {jobListings[employer.id] && jobListings[employer.id].length > 0 ? (
+                jobListings[employer.id].map((job) => (
+                  <div className="job" key={job.id}>
+                    <h3>
+                      <a href={`/job/${job.pk}`}>{job.jobtitle}</a>
+                    </h3>
+                    <p>
+                      <strong>Location:</strong> {job.location}
+                    </p>
+                    <p>
+                      <strong>Salary:</strong> {job.salary}
+                    </p>
+                    <p>
+                      <strong>Requirements:</strong> {job.jobrequirements}
+                    </p>
+                    <p>
+                      <strong>Description:</strong> {job.description}
+                    </p>
+                  </div>
+                ))
+              ) : (
+                <p>No job listings found.</p>
+              )}
+            </div>
+          </div>
+        </section>
+      ))}
     </div>
   );
 };
