e25363f823fca923cf5b304e6b2937e727c59061
tavice <thomasavice.ta@gmail.comm>
2023-05-13 16:22:27 -0700

    fetched using axios

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -12,12 +12,14 @@ import EmployerList from './pages/EmployerList';
 import JobListingDetail from './pages/JobListingDetail';
 
 function App() {
+  const baseUrl = 'http://127.0.0.1:8000'
+
   return (
     <div className="App">
       <Header />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/employer-job-listing" element={<EmployerList />} />
+        <Route path="/employer-job-listing" element={<EmployerList baseUrl={baseUrl} />} />
         <Route path="/job-listing-detail/:id" element={<JobListingDetail />} />
       </Routes>
       <Footer />
--- frontend/src/pages/EmployerList.js
+++ frontend/src/pages/EmployerList.js
@@ -1,9 +1,39 @@
-import React from 'react'
+import { useEffect } from "react";
+import { useState } from "react";
+import axios from "axios";
 
-const EmployerList = () => {
+const EmployerList = ({ baseUrl }) => {
+
+  //Fetch data from backend using axios and useEffect
+  const [employerList, setEmployerList] = useState([]);
+
+  const fetchEmployerList = async () => {
+    const response = await axios.get(`${baseUrl}/api/employers/?format=json`);
+    const data = response.data;
+    setEmployerList(data);
+  };
+
+  console.log(employerList);
+
+  useEffect(() => {
+    fetchEmployerList();
+  }, []);
+
   return (
-    <div>EmployerList</div>
-  )
-}
+    <div>
+      <h1>Employer List</h1>
+      <ul>
+        {employerList.map((employer) => {
+          return (
+            <li key={employer.id}>
+              <h2>{employer.companyname}</h2>
+              <p>{employer.website}</p>
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
+};
 
-export default EmployerList
+export default EmployerList;
