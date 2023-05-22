6a41bed98883a2fd47b1e945019ef194195e3ae3
tavice <thomasavice.ta@gmail.com>
2023-05-22 11:44:10 -0700

    keep working on listofjobsyoupsoted with conditional rendering

--- frontend/src/components/ListOfJobsYouPosted.js
+++ frontend/src/components/ListOfJobsYouPosted.js
@@ -1,5 +1,9 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { Link } from "react-router-dom";
+import { styled } from "@mui/material/styles";
+import Typography from "@mui/material/Typography";
+import { Button } from "@mui/material";
 
 const ListOfJobsYouPosted = ({ baseUrl }) => {
   const [currentUser, setCurrentUser] = useState({});
@@ -10,7 +14,9 @@ const ListOfJobsYouPosted = ({ baseUrl }) => {
   useEffect(() => {
     const fetchCurrentUser = async () => {
       try {
-        const response = await axios.get(`${baseUrl}/api/user/${localStorage.getItem('authenticated_user')}`);
+        const response = await axios.get(
+          `${baseUrl}/api/user/${localStorage.getItem("authenticated_user")}`
+        );
         const data = response.data;
         setCurrentUser(data);
       } catch (error) {
@@ -30,7 +36,7 @@ const ListOfJobsYouPosted = ({ baseUrl }) => {
         if (foundEmployer) {
           setEmployer(foundEmployer);
         } else {
-          console.log('Employer not found');
+          console.log("Employer not found");
         }
       } catch (error) {
         console.log(error);
@@ -55,22 +61,49 @@ const ListOfJobsYouPosted = ({ baseUrl }) => {
   }, []);
 
   useEffect(() => {
-    const filteredJobListings = jobListings.filter((item) => item.employer === (employer ? employer.id : null));
+    const filteredJobListings = jobListings.filter(
+      (item) => item.employer === (employer ? employer.id : null)
+    );
     setFilteredJobListings(filteredJobListings);
   }, [employer, jobListings]);
 
-  console.log('filtered job listings are', filteredJobListings);
+  console.log("filtered job listings are", filteredJobListings);
+
+  //=======================================================//
+  //Styling//
+
+  const StyledLink = styled(Link)`
+    color: #000;
+
+    text-transform: uppercase;
+    font-size: 1.2rem;
+    margin: 0 1rem;
+  `;
+
+  //=======================================================//
+  //Render//
 
   return (
     <div>
-      {filteredJobListings.map((listing) => (
-        <div key={listing.id}>
-          <h3>{listing.jobtitle}</h3>
-          <p>{listing.description}</p>
-          <p>{listing.location}</p>
-          <p>{listing.salary}</p>
-        </div>
-      ))}
+      <div>
+        {filteredJobListings.length === 0 ? (
+          <Typography>You have not posted any jobs yet</Typography>
+        ) : (
+          <div>
+            {filteredJobListings.map((listing) => (
+              <div key={listing.id}>
+                <Typography>
+                  {" "}
+                  <StyledLink to={`/Job Listings/${listing.id}`}>
+                    {listing.jobtitle}{" "}
+                  </StyledLink>
+                </Typography>
+              </div>
+            ))}
+          </div>
+        )}
+      </div>
+      <Button component={Link} to="/create-your-job-offer">Create your next Job Offer !</Button>
     </div>
   );
 };
