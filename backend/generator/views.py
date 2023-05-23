e30c934e3c69f597afface51d3600923c7beb652
tavice <thomasavice.ta@gmail.com>
2023-05-22 21:14:51 -0700

    added styling to the chat

--- backend/generator/views.py
+++ backend/generator/views.py
@@ -8,65 +8,75 @@ import os
 import json
 
 OPENAI_API_KEY = str(os.getenv("OPENAI_API_KEY"))
-#print('OPENAI_API_KEY is', OPENAI_API_KEY)
+# print('OPENAI_API_KEY is', OPENAI_API_KEY)
 openai.api_key = OPENAI_API_KEY
+
 
 @require_POST
 @csrf_exempt
 def chat(request):
     try:
         # if the session does not have a messages key, create one
-        if 'messages' not in request.session:
-            request.session['messages'] = [
-                {"role": "system", "content": "You are now chatting with a job recruiter. The recruiter will provide you with comprehensive, clear, and cordial answers regarding job offers."},
+        if "messages" not in request.session:
+            request.session["messages"] = [
+                {
+                    "role": "system",
+                    "content": "You are now chatting with a job recruiter. The recruiter will provide you with comprehensive, clear, and cordial answers regarding job offers.",
+                },
             ]
 
-        if request.method == 'POST':
-            messages = request.session['messages']
+        if request.method == "POST":
+            messages = request.session["messages"]
             body = json.loads(request.body)
-            prompt = body.get('prompt')
-            temperature = body.get('temperature')
+            prompt = body.get("prompt")
+            temperature = body.get("temperature")
 
-            # check what is the prompt
-            print('prompt is', prompt)
-            print('temperature is', temperature)
-            print('messages are', messages)
-
+            # Append user message to messages
             messages.append({"role": "user", "content": prompt})
+
+            # Format messages for OpenAI API
+            formatted_messages = [
+                {"role": message["role"], "content": message["content"]}
+                for message in messages
+            ]
+
+            formatted_messages.append({"role": "user", "content": prompt})
+
+            print("formatted_messages are", formatted_messages)
 
             response = openai.ChatCompletion.create(
                 model="gpt-3.5-turbo",
-                messages=messages,
+                messages=formatted_messages,
                 temperature=temperature,
-                max_tokens=200,
+                max_tokens=1000,
             )
 
-            formatted_response = response['choices'][0]['message']['content']
+            formatted_response = response["choices"][0]["message"]["content"]
             messages.append({"role": "assistant", "content": formatted_response})
-            request.session['messages'] = messages
+            request.session["messages"] = messages
 
-            print('formatted_response is', formatted_response)
-            print('messages are', messages)
+            print("formatted_response is", formatted_response)
+            print("messages are", messages)
 
             return JsonResponse(
                 {
-                    #'messages': messages,
-                    'formattedResponse': formatted_response,
+                    'messages': messages,
+                    "formattedResponse": formatted_response,
                 }
             )
         else:
-            return JsonResponse({'error': 'Invalid request'})
+            return JsonResponse({"error": "Invalid request"})
     except Exception as e:
         print(e)
-        return JsonResponse({'error': str(e)})
-    
+        return JsonResponse({"error": str(e)})
 
-    #===========================================================================#
-    #Clear the chat
+    # ===========================================================================#
+    # Clear the chat
+
 
 @require_POST
 @csrf_exempt
 def clear_chat(request):
-    if 'messages' in request.session:
-        del request.session['messages']
-    return JsonResponse({'status': 'success'})
+    if "messages" in request.session:
+        del request.session["messages"]
+    return JsonResponse({"status": "success"})
--- frontend/src/pages/OfferGPT.js
+++ frontend/src/pages/OfferGPT.js
@@ -3,18 +3,14 @@ import axios from "axios";
 import { Button, Typography, Grid, Paper } from "@mui/material";
 import Container from "@mui/material/Container";
 import Box from "@mui/material/Box";
-import PersonIcon from "@mui/icons-material/Person";
-import HandshakeIcon from "@mui/icons-material/Handshake";
-import WorkspacePremiumIcon from "@mui/icons-material/WorkspacePremium";
-import AllInclusiveIcon from "@mui/icons-material/AllInclusive";
-import SpaIcon from "@mui/icons-material/Spa";
-import PhoneInTalkIcon from "@mui/icons-material/PhoneInTalk";
-//import { useHistory } from "react-router-dom";
+import TextField from "@mui/material/TextField";
+import CircularProgress from "@mui/material/CircularProgress";
 
 const OfferGPT = ({ baseUrl }) => {
   const [messages, setMessages] = useState([]);
   const [prompt, setPrompt] = useState("");
   const [temperature, setTemperature] = useState(0.1);
+  const [loading, setLoading] = useState(false); // Add loading state
 
   //console.log(`baseUrl: ${baseUrl}`);
   //const history = useHistory();
@@ -24,6 +20,7 @@ const OfferGPT = ({ baseUrl }) => {
 
   const handleSubmit = async (event) => {
     event.preventDefault();
+    setLoading(true); // Set loading state to true
     console.log(`prompt: ${prompt}`);
     console.log(`temperature: ${temperature}`);
 
@@ -59,6 +56,8 @@ const OfferGPT = ({ baseUrl }) => {
     } catch (error) {
       console.error("Error:", error);
       // Handle the error as needed
+    } finally {
+      setLoading(false); // Set loading state to false after response is received
     }
   };
 
@@ -106,57 +105,65 @@ const OfferGPT = ({ baseUrl }) => {
   return (
     <Container maxWidth="lg" sx={{ mt: 4, mb: 4 }}>
       <Typography variant="h4" style={{ marginBottom: 10 }}>
-        JobGPT
+        JobGPT your Assistant to create amazing job descriptions
       </Typography>
-
-      {/* <button type="button" onClick={() => history.push("/new_chat")}>
-        New Chat +
-      </button> */}
 
       <div className="chat-history">
         {messages.map((message, index) => (
           <Paper
-            style={{ padding: 20 }}
+            style={{ padding: 40 }}
             key={index}
             className="answer-discussion"
           >
-            <Paper>
+            <Paper elevation={4} style={{ padding: 20 }}>
               <strong>{message.role}</strong>: {message.content}
-            </Paper>
-            <Paper>
-              <strong>Assistant:</strong> {message.content.answer}
             </Paper>
           </Paper>
         ))}
       </div>
-      <form onSubmit={handleSubmit}>
-        <input
+      <form
+        onSubmit={handleSubmit}
+        style={{ display: "flex", padding: 20, marginTop: 20 }}
+      >
+        <TextField
           required
           type="text"
-          placeholder="enter prompt"
+          placeholder="Type anything here... give me a software developer job description!"
           autoFocus
           name="prompt"
           value={prompt}
+          variant="outlined"
+          multiline
+          style={{ width: "100%", marginRight: 20 }}
           onChange={handlePromptChange}
         />
-        <label htmlFor="temperature" style={{ color: "black" }}>
-          Temperature:
-        </label>
-        <input
-          type="number"
-          step="0.01"
-          min="0"
-          max="2"
-          name="temperature"
-          value={temperature}
-          onChange={handleTemperatureChange}
-          id="temperature"
-        />
-        <button type="submit">GENERATE</button>
-        <button type="button" onClick={handleClearChat}>
-          CLEAR CHAT
-        </button>
+        <Button type="submit" style={{ width: "50%" }}>
+          {loading ? (
+            <CircularProgress size={20} color="secondary" /> // Show spinner while loading
+          ) : (
+            "GENERATE"
+          )}
+        </Button>
+        <div />
+        <div>
+          <label htmlFor="temperature" style={{ color: "black" }}>
+            Temperature:
+          </label>
+          <input
+            type="number"
+            step="0.01"
+            min="0"
+            max="2"
+            name="temperature"
+            value={temperature}
+            onChange={handleTemperatureChange}
+            id="temperature"
+          />
+        </div>
       </form>
+      <Button type="button" onClick={handleClearChat}>
+        CLEAR CHAT
+      </Button>
     </Container>
   );
 };
