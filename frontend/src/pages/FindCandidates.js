5f765152c50c932cae0162816ca44372d37fa14d
tavice <thomasavice.ta@gmail.com>
2023-06-01 16:41:42 -0700

    added resume fetch

--- frontend/src/pages/FindCandidates.js
+++ frontend/src/pages/FindCandidates.js
@@ -7,6 +7,9 @@ import Container from "@mui/material/Container";
 const FindCandidates = ({ baseUrl }) => {
   const [jobseeker, setJobseeker] = useState([]);
   const [user, setUser] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(null);
+  const [resume, setResume] = useState([]);
 
   // Fetch all the job seekers
   const fetchJobseeker = async () => {
@@ -34,8 +37,11 @@ const FindCandidates = ({ baseUrl }) => {
       );
 
       setUser(filteredUser);
+      setIsLoading(false);
     } catch (error) {
       console.log(error);
+      setError(error.message);
+      setIsLoading(false);
     }
   };
 
@@ -43,7 +49,29 @@ const FindCandidates = ({ baseUrl }) => {
     fetchUser();
   }, []);
 
+  // Fetch all the resumes
+  const fetchResume = async () => {
+    try {
+      const response = await axios.get(`${baseUrl}/api/resume`);
+      const data = response.data;
+      setResume(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchResume();
+  }, []);
+
   // Render the list of job seekers
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+  if (error) {
+    return <div>{error}</div>;
+  }
+
   return (
     <Container maxWidth="lg" sx={{ mt: 4, mb: 4 }}>
       <Typography variant="h3" gutterBottom>
@@ -52,6 +80,30 @@ const FindCandidates = ({ baseUrl }) => {
       <Grid container spacing={2}>
         {jobseeker.map((jobseeker) => {
           const userMatch = user.find((user) => user.id === jobseeker.user);
+          const resumeMatch = resume.find(
+            (resume) => resume.jobseeker === jobseeker.id
+          );
+
+          if (!resumeMatch) {
+            return (
+              <Grid item xs={12} sm={6} key={userMatch.id}>
+                <Paper elevation={3} sx={{ p: 2 }}>
+                  <Typography variant="h6" gutterBottom>
+                    {userMatch.first_name} {userMatch.last_name}
+                  </Typography>
+                  <Typography variant="body1" gutterBottom>
+                    {jobseeker.bio}
+                  </Typography>
+                  <Typography variant="body1" gutterBottom>
+                    Email: {userMatch.email}
+                  </Typography>
+                  <Typography variant="body1" gutterBottom>
+                    Skills: Resume not shared yet
+                  </Typography>
+                </Paper>
+              </Grid>
+            );
+          }
 
           if (userMatch) {
             return (
@@ -66,10 +118,14 @@ const FindCandidates = ({ baseUrl }) => {
                   <Typography variant="body1" gutterBottom>
                     Email: {userMatch.email}
                   </Typography>
+                  <Typography variant="body1" gutterBottom>
+                    Skills: {resumeMatch.skills}
+                  </Typography>
                 </Paper>
               </Grid>
             );
           }
+
           return null;
         })}
       </Grid>
