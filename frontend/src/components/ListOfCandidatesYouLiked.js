70932b37c83c1aafe65f2f157ae91b983223eaf8
tavice <thomasavice.ta@gmail.com>
2023-06-03 12:09:27 -0700

    used promise all for optimized API call

--- frontend/src/components/ListOfCandidatesYouLiked.js
+++ frontend/src/components/ListOfCandidatesYouLiked.js
@@ -4,8 +4,6 @@ import Container from "@mui/material/Container";
 import Grid from "@mui/material/Grid";
 import { Link } from "react-router-dom";
 import Typography from "@mui/material/Typography";
-import Chip from "@mui/material/Chip";
-import Stack from "@mui/material/Stack";
 
 const ListOfCandidatesYouLiked = ({ baseUrl }) => {
   const employerId = localStorage.getItem("employer_id");
@@ -13,17 +11,33 @@ const ListOfCandidatesYouLiked = ({ baseUrl }) => {
   const [isLoading, setIsLoading] = useState(true);
   const [error, setError] = useState(null);
   const [jobSeekers, setJobSeekers] = useState([]);
+  const [users, setUsers] = useState([]);
 
-  // Fetch all the candidates that the employer liked
-  const fetchCandidates = async () => {
+  //Combine the API calls to fetch candidates, job seekers, and users into a single function to reduce the number of requests.
+
+  // Use async/await with Promise.all() to make parallel API requests for faster data retrieval.
+  //https://www.geeksforgeeks.org/explain-promise-all-with-async-await-in-javascript/
+
+  const fetchData = async () => {
     try {
-      const response = await axios.get(`${baseUrl}/api/savedcandidates/`);
-      const data = response.data;
-      const filteredData = data.filter(
+      const [candidatesResponse, jobSeekersResponse, usersResponse] =
+        await Promise.all([
+          axios.get(`${baseUrl}/api/savedcandidates/`),
+          axios.get(`${baseUrl}/api/jobseekers/`),
+          axios.get(`${baseUrl}/api/user/`),
+        ]);
+
+      const candidatesData = candidatesResponse.data;
+      const jobSeekersData = jobSeekersResponse.data;
+      const usersData = usersResponse.data;
+
+      const filteredCandidates = candidatesData.filter(
         (item) => parseInt(item.employer) === parseInt(employerId)
       );
 
-      setCandidates(filteredData);
+      setCandidates(filteredCandidates);
+      setJobSeekers(jobSeekersData);
+      setUsers(usersData);
       setIsLoading(false);
     } catch (error) {
       console.log(error);
@@ -33,55 +47,8 @@ const ListOfCandidatesYouLiked = ({ baseUrl }) => {
   };
 
   useEffect(() => {
-    fetchCandidates();
+    fetchData();
   }, []);
-
-  // Fetch all the job seekers associated with the candidates
-  const fetchJobSeekers = async () => {
-    try {
-      const response = await axios.get(`${baseUrl}/api/jobseekers/`);
-      const data = response.data;
-      const filteredData = data.filter((item) =>
-        candidates.some((application) => application.job_seeker === item.id)
-      );
-      setJobSeekers(filteredData);
-    } catch (error) {
-      console.log(error);
-    }
-  };
-
-
-  useEffect(() => {
-    fetchJobSeekers();
-  }, []);
-
-  //fetch all the users associated with the job seekers
-  const [users, setUsers] = useState([]);
-    const fetchUsers = async () => {
-        try {
-            const response = await axios.get(`${baseUrl}/api/user/`);
-            const data = response.data;
-            const filteredData = data.filter((item) =>
-            jobSeekers.some((application) => application.user === item.id)
-            );
-            setUsers(filteredData);
-        } catch (error) {
-            console.log(error);
-        }
-    };
-
-    useEffect(() => {
-        fetchUsers();
-    }, [jobSeekers]);
-
-  console.log("candidates are", candidates);
-  console.log("job seekers are", jobSeekers);
-  console.log("users are", users);
-
-  //Combine the API calls to fetch candidates, job seekers, and users into a single function to reduce the number of requests.
-
-// Use async/await with Promise.all() to make parallel API requests for faster data retrieval.
-//https://www.geeksforgeeks.org/explain-promise-all-with-async-await-in-javascript/
 
   // Render
   if (isLoading) {
@@ -94,32 +61,32 @@ const ListOfCandidatesYouLiked = ({ baseUrl }) => {
 
   return (
     <Container maxWidth="lg" sx={{ mt: 4, mb: 4 }}>
-     
-      {users.map((user) => (
-        <Grid container spacing={2} key={user.id}>
-          <Grid item xs={12} sm={6}>
-            <Typography variant="h6" gutterBottom>
-            <Link to={`/find-candidates/${user.id}`}> {user.first_name} {user.last_name}:  </Link>
-            </Typography>
+      {candidates.map((candidate) => {
+        const jobSeeker = jobSeekers.find(
+          (jobSeeker) => jobSeeker.id === candidate.job_seeker
+        );
+
+        const user = users.find((user) => user.id === jobSeeker?.user);
+
+        return (
+          <Grid container spacing={2} key={candidate.id}>
+            <Grid item xs={12} sm={6}>
+              <Typography variant="h6" gutterBottom>
+                <Link to={`/find-candidates/${user?.id}`}>
+                  {user?.first_name} {user?.last_name}:
+                </Link>
+              </Typography>
+            </Grid>
+            <Grid item xs={12} sm={6}>
+              <Typography variant="body1" gutterBottom>
+                {jobSeeker?.bio}
+              </Typography>
+            </Grid>
           </Grid>
-          {jobSeekers.map((jobSeeker) => {
-            if (jobSeeker.user === user.id) {
-              return (
-                <Grid item xs={12} sm={6} key={jobSeeker.id}>
-                  <Typography variant="body1" gutterBottom>
-                    {jobSeeker.bio}
-                  </Typography>
-                
-                </Grid>
-              );
-            }
-            return null;
-          })}
-        </Grid>
-      ))}
+        );
+      })}
     </Container>
   );
-  
 };
 
 export default ListOfCandidatesYouLiked;
