8f406ccfea5db65b8c43e66f88ff5baa0c16a76b
tavice <thomasavice.ta@gmail.com>
2023-06-05 16:48:51 -0700

    displaye candidate who applied in a tab

--- frontend/src/components/CandidatesWhoAppliedToYourJob.js
+++ frontend/src/components/CandidatesWhoAppliedToYourJob.js
@@ -1,9 +1,17 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-import { Link } from "react-router-dom";
-import { styled } from "@mui/material/styles";
+import Container from "@mui/material/Container";
+import Grid from "@mui/material/Grid";
 import Typography from "@mui/material/Typography";
-import { Button } from "@mui/material";
+import Chip from "@mui/material/Chip";
+import Stack from "@mui/material/Stack";
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
 
 const CandidatesWhoAppliedToYourJob = ({ baseUrl }) => {
   const [jobApplications, setJobApplications] = useState([]);
@@ -101,7 +109,51 @@ const CandidatesWhoAppliedToYourJob = ({ baseUrl }) => {
 
 
 
-  return <div>CandidatesWhoAppliedToYourJob</div>;
+    return (
+        <Container maxWidth="lg" sx={{ mt: 4, mb: 4 }}>
+          <Grid container spacing={3}>
+            <Grid item xs={12}>
+              <TableContainer component={Paper}>
+                <Table sx={{ minWidth: 650 }} aria-label="simple table">
+                  <TableHead>
+                    <TableRow>
+                      <TableCell>Job Title</TableCell>
+                      <TableCell align="right">Name</TableCell>
+                      <TableCell align="right">Bio</TableCell>
+                      <TableCell align="right">Status</TableCell>
+                    </TableRow>
+                  </TableHead>
+                  <TableBody>
+                    {jobApplications.map((jobApplication) => {
+                      const jobSeeker = jobSeekers.find(
+                        (jobSeeker) => jobSeeker.id === jobApplication.job_seeker
+                      );
+                      const userMatch = user.find((user) => user.id === jobSeeker.user);
+                      const jobListing = jobListings.find(
+                        (jobListing) => jobListing.id === jobApplication.job_listing
+                      );
+    
+                      return (
+                        <TableRow key={jobApplication.id}>
+                          <TableCell component="th" scope="row" style={{textTransform:"uppercase", fontWeight:"bold"}}>
+                            {jobListing.jobtitle}
+                          </TableCell>
+                          <TableCell align="right">{userMatch && userMatch.first_name}{" "}{userMatch && userMatch.last_name}</TableCell>
+                          <TableCell align="right">{jobSeeker && jobSeeker.bio}</TableCell>
+                          <TableCell align="right">
+                            <Chip label={jobApplication.application_status} variant="outlined" />
+                          </TableCell>
+                        </TableRow>
+                      );
+                    })}
+                  </TableBody>
+                </Table>
+              </TableContainer>
+            </Grid>
+          </Grid>
+        </Container>
+      );
+      
 };
 
 export default CandidatesWhoAppliedToYourJob;
