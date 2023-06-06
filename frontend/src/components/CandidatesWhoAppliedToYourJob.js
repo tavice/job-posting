a4b052acf622787c517a2a8ce85bcc13f7afaeb8
tavice <thomasavice.ta@gmail.com>
2023-06-05 18:07:03 -0700

    button to change status started working on it

--- frontend/src/components/CandidatesWhoAppliedToYourJob.js
+++ frontend/src/components/CandidatesWhoAppliedToYourJob.js
@@ -1,18 +1,26 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import axios from "axios";
 import Container from "@mui/material/Container";
 import Grid from "@mui/material/Grid";
 import Typography from "@mui/material/Typography";
-import Chip from "@mui/material/Chip";
-import Stack from "@mui/material/Stack";
+import Button from "@mui/material/Button";
+import ButtonGroup from "@mui/material/ButtonGroup";
+import ArrowDropDownIcon from "@mui/icons-material/ArrowDropDown";
+import ClickAwayListener from "@mui/material/ClickAwayListener";
+import Grow from "@mui/material/Grow";
+import Paper from "@mui/material/Paper";
+import Popper from "@mui/material/Popper";
+import MenuItem from "@mui/material/MenuItem";
+import MenuList from "@mui/material/MenuList";
 import Table from "@mui/material/Table";
 import TableBody from "@mui/material/TableBody";
 import TableCell from "@mui/material/TableCell";
 import TableContainer from "@mui/material/TableContainer";
 import TableHead from "@mui/material/TableHead";
 import TableRow from "@mui/material/TableRow";
-import Paper from "@mui/material/Paper";
 import { Link } from "react-router-dom";
+
+const options = ["Pending", "Approved", "Rejected"];
 
 const CandidatesWhoAppliedToYourJob = ({ baseUrl }) => {
   const [jobApplications, setJobApplications] = useState([]);
@@ -21,11 +29,13 @@ const CandidatesWhoAppliedToYourJob = ({ baseUrl }) => {
   const [user, setUser] = useState({});
   const [isLoading, setIsLoading] = useState(true);
   const [error, setError] = useState(null);
+  const [open, setOpen] = useState(false); //https://mui.com/material-ui/react-button-group/#split-button
+  const anchorRef = useRef(null);
+  const [selectedIndex, setSelectedIndex] = useState(1);
 
   // Get employer id
   const employerId = localStorage.getItem("employer_id");
 
-  //fetch all the data using Promise.all (need job listings, job applications, job seekers, user) and filter them
   useEffect(() => {
     const fetchData = async () => {
       try {
@@ -80,8 +90,47 @@ const CandidatesWhoAppliedToYourJob = ({ baseUrl }) => {
     fetchData();
   }, [baseUrl, employerId]);
 
-  //===============================================================================================================
-  //render
+  const handleChangeStatus = async (status, applicationId) => {
+    try {
+      await axios.put(`${baseUrl}/api/jobapplications/${applicationId}/`, {
+        application_status: status,
+      });
+      // Update the jobApplications state to reflect the updated status
+      const updatedApplications = jobApplications.map((application) =>
+        application.id === applicationId
+          ? { ...application, application_status: status }
+          : application
+      );
+      setJobApplications(updatedApplications);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+//===================================================
+//button to change status
+const handleClick = () => {
+    console.info(`You clicked ${options[selectedIndex]}`);
+  };
+
+  const handleMenuItemClick = (event, index) => {
+    setSelectedIndex(index);
+    setOpen(false);
+  };
+
+  const handleToggle = () => {
+    setOpen((prevOpen) => !prevOpen);
+  };
+
+  const handleClose = (event) => {
+    if (anchorRef.current && anchorRef.current.contains(event.target)) {
+      return;
+    }
+
+    setOpen(false);
+  };
+  //================================================================
+  // Render
 
   if (isLoading) {
     return <div>Loading...</div>;
@@ -101,19 +150,20 @@ const CandidatesWhoAppliedToYourJob = ({ baseUrl }) => {
         <Grid item xs={12}>
           <TableContainer component={Paper}>
             <Table sx={{ minWidth: 650 }} aria-label="simple table">
-              <TableHead  style={{
-                          textTransform: "uppercase",
-                          fontWeight: "bold",
-                        }}>
+              <TableHead
+                style={{
+                  textTransform: "uppercase",
+                  fontWeight: "bold",
+                }}
+              >
                 <TableRow>
-                  <TableCell>Job Title</TableCell>
+                  <TableCell align="center">Job Title</TableCell>
                   <TableCell align="center">Name</TableCell>
-
                   <TableCell align="center">Status</TableCell>
                 </TableRow>
               </TableHead>
               <TableBody>
-                {jobApplications.map((jobApplication) => {
+                {jobApplications.map((jobApplication, index) => {
                   const jobSeeker = jobSeekers.find(
                     (jobSeeker) => jobSeeker.id === jobApplication.job_seeker
                   );
@@ -125,48 +175,101 @@ const CandidatesWhoAppliedToYourJob = ({ baseUrl }) => {
                   );
 
                   const status = jobApplication
-                  ? jobApplication.application_status
-                  : "Unknown Status";
-                let chipColor;
-        
-                if (status === "Pending") {
-                  chipColor = "warning";
-                } else if (status === "Approved") {
-                  chipColor = "success";
-                } else if (status === "Rejected") {
-                  chipColor = "error";
-                } else {
-                  chipColor = "default";
-                }
+                    ? jobApplication.application_status
+                    : "Unknown Status";
+                  let chipColor;
+
+                  if (status === "Pending") {
+                    chipColor = "warning";
+                  } else if (status === "Approved") {
+                    chipColor = "success";
+                  } else if (status === "Rejected") {
+                    chipColor = "error";
+                  } else {
+                    chipColor = "default";
+                  }
+
+                  const selectedStatus =
+                    selectedIndex === index ? options[selectedIndex] : null;
 
                   return (
                     <TableRow key={jobApplication.id}>
                       <TableCell
+                        align="center"
                         component="th"
                         scope="row"
                         style={{
                           textTransform: "uppercase",
                           fontWeight: "bold",
                         }}
-                      
                       >
-                        <Link to={`/Job Listings/${jobListing.id}`} style={{textDecoration:"none"}}>
-
-                        {jobListing.jobtitle}
+                        <Link
+                          to={`/Job Listings/${jobListing.id}`}
+                          style={{ textDecoration: "none" }}
+                        >
+                          {jobListing.jobtitle}
                         </Link>
                       </TableCell>
                       <TableCell align="center">
-                      <Link to={`/find-candidates/${userMatch?.id}`} style={{textDecoration:"none"}}>
-                        {userMatch && userMatch.first_name}{" "}
-                        {userMatch && userMatch.last_name}
+                        <Link
+                          to={`/find-candidates/${userMatch?.id}`}
+                          style={{ textDecoration: "none" }}
+                        >
+                          {userMatch && userMatch.first_name}{" "}
+                          {userMatch && userMatch.last_name}
                         </Link>
                       </TableCell>
                       <TableCell align="center">
-                        <Chip
-                          label={jobApplication.application_status}
-                         
-                          color={chipColor}
-                        />
+                      <ButtonGroup variant="contained" ref={anchorRef} aria-label="split button">
+        <Button onClick={handleClick}>{options[selectedIndex]}</Button>
+        <Button
+          size="small"
+          aria-controls={open ? 'split-button-menu' : undefined}
+          aria-expanded={open ? 'true' : undefined}
+          aria-label="select merge strategy"
+          aria-haspopup="menu"
+          onClick={handleToggle}
+        >
+          <ArrowDropDownIcon />
+        </Button>
+      </ButtonGroup>
+      <Popper
+        sx={{
+          zIndex: 1,
+        }}
+        open={open}
+        anchorEl={anchorRef.current}
+        role={undefined}
+        transition
+        disablePortal
+      >
+        {({ TransitionProps, placement }) => (
+          <Grow
+            {...TransitionProps}
+            style={{
+              transformOrigin:
+                placement === 'bottom' ? 'center top' : 'center bottom',
+            }}
+          >
+            <Paper>
+              <ClickAwayListener onClickAway={handleClose}>
+                <MenuList id="split-button-menu" autoFocusItem>
+                  {options.map((option, index) => (
+                    <MenuItem
+                      key={option}
+                      disabled={index === 2}
+                      selected={index === selectedIndex}
+                      onClick={(event) => handleMenuItemClick(event, index)}
+                    >
+                      {option}
+                    </MenuItem>
+                  ))}
+                </MenuList>
+              </ClickAwayListener>
+            </Paper>
+          </Grow>
+        )}
+      </Popper>
                       </TableCell>
                     </TableRow>
                   );
